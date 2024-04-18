6da6ab4d6e2e8df3ce1ea700e3527b137535e414
Wande <wanderlustking11@gmail.com>
2024-04-18 14:01:27 -0400

    Add Tailwind CSS; create app structure

--- README.md
+++ README.md
@@ -7,6 +7,28 @@ This was built upon the Vite template, which makes this app run smooth and fast!
 
 ## Spreading managed democracy across the galaxy
 
-If you're a fan of the game Helldivers 2, test your knowledge and see if you're a true patriot of freedom and liberty, or if you're exhibiting sympathetic tendencies inclined to the bugs and bots.
+If you're a fan of the game Helldivers 2, test your knowledge and see if you're a true patriot of freedom and liberty, or if you're exhibiting sympathetic tendencies inclined to the Terminids and Automatons.
 
-### Warning: Traitors will be reported to the of Re-education
+**_ Warning: Traitors will be reported to the Ministry of Re-education _**
+
+### Current Implementations
+
+- This is a Single Page Application
+- Uses libraries such as:
+  - Tailwind CSS
+  - Framer-Motion
+  - (See dependencies in the [package.json file](https://github.com/WanderlustKing11/react-trivia/blob/main/package.json))
+- No current sliders, just 'Next' and 'Prev' buttons that trigger animation to move forward or back on a selected trivia card.
+- One custom button allows the user to reveal the card's answer.
+- If you like, you can keep track of your score whenever your ideologies align with Super-Earth's with another custom Button Component that keeps track of the score count out of how many trivia questions you have answered. This means you are responsible for managing your own score, for now. But Super-Earth's Ministry of Truth will be updating a better tracking system in the future.
+- One final Button Component to Restart the game.
+
+#### Future Implementations
+
+- Title Page --> Gives better instruction and UX for players to enter the game
+- Styling
+- Images
+- Responsiveness, more Mobile friendly
+- Score tracking system
+- Possible multiple choice answers
+- Breadcrumbs to keep track of the player's progress
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BsArrowLeftSquare, BsArrowRightSquare } from 'react-icons/bs';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-screen h-screen p-10'>
+      <div className='flex justify-end'>
+        <button className='bg-black rounded-full p-4'>Restart</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      {/* <div className='flex justify-center'></div> */}
+      <h1 className='text-4xl font-bold my-20 flex justify-center'>
+        HellDivers 2 Trivia
+      </h1>
+      <p className='flex justify-center'>
+        Say hello to DEMOCRACY, and help spread FREEDOM across the galaxy by
+        answering these trivia questions:
       </p>
-    </>
-  )
+      <div className='flex justify-center p-8'>
+        <div className='flex w-auto h-auto grid grid-cols-3 gap-11 content-center justify-self-center'>
+          <button className='flex grid justify-center content-center'>
+            <BsArrowLeftSquare className='w-10 h-10' />
+          </button>
+          <div className='w-52 h-80 text-white bg-black flex grid justify-center content-center'>
+            Card
+          </div>
+          <button className='flex grid justify-center content-center'>
+            <BsArrowRightSquare className='w-10 h-10' />
+          </button>
+        </div>
+      </div>
+      <div className='flex w-auto h-auto grid grid-cols-3 gap-11 content-center justify-self-center'>
+        <div className='grid'></div>
+        <button className='flex grid justify-center border-solid border-2 rounded-lg'>
+          Reveal
+        </button>
+        <div className='grid'></div>
+      </div>
+      <div className='w-1/6 h-1/6 border-solid border-2'>
+        <div className='flex justify-center py-8 text-lg font-bold'>
+          Score: 0/20
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
